150b437f747d2a8889e3643196df2256367bbc28
Macbook Pro <macbookpro@MacBook-Pro-2.local>
2018-10-21 18:09:54 +0300

    init commit

---
+++ swift-shopper/swift-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-shopper
+//
+//  Created by Macbook Pro on 21/10/2018.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
